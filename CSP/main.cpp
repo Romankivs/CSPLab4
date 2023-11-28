894ec7441e28919b42cc16c16d26fedce16034e3
Sviatoslav Romankiv <sviatoslav@source-elements.com>
2023-11-28 20:59:16 +0200

    Initial Commit

---
+++ CSP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSP
+//
+//  Created by Sviatoslav Romankiv on 28.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
